315cb40871426498e9ac9d0d48efea2f31abc95a
Mech <stefrensma@gmail.com>
2024-04-01 17:33:24 +0200

    Added package presentation and lowercased all other packages Review api call still not working

--- app/src/main/java/com/example/filmapp/activities/ReviewOverviewActivity.java
+++ app/src/main/java/com/example/filmapp/activities/ReviewOverviewActivity.java
@@ -17,7 +17,7 @@ import com.example.filmapp.api.ApiInterface;
 import com.example.filmapp.api.RetrofitClient;
 import com.example.filmapp.api.response.MovieReviewResponse;
 import com.example.filmapp.model.MovieReview;
-
+import com.example.filmapp.Activities.MainActivity;
 import java.util.ArrayList;
 import java.util.List;
 
