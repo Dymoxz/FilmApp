82706bb6b897413c25772e279fa1de1817b1543b
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-04-02 00:46:25 +0200

    Kleine dingetjes opgelost bij carousel

--- app/src/main/java/com/example/filmapp/activities/MovieDetailActivity.java
+++ app/src/main/java/com/example/filmapp/activities/MovieDetailActivity.java
@@ -139,7 +139,7 @@ public class MovieDetailActivity extends AppCompatActivity {
                             trailer.setMovieId(movieId);
                             videoViewModel.insertVideo(trailer);
                             WebView webView = new WebView(getApplicationContext());
-                            String embedLink = "<iframe width=\"560\" height=\"315\" src=\"" + trailerUrl + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+                            String embedLink = "<iframe width=\"100%\" height=\"315\" src=\"" + trailerUrl + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
                             webView.loadData(embedLink, "text/html", "utf-8");
                             webView.getSettings().setJavaScriptEnabled(true);
                             webView.setWebChromeClient(new WebChromeClient());
