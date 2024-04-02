376500a7ea8e78f5de1c7bbda4a533cac8dbff92
Mech <stefrensma@gmail.com>
2024-04-02 21:20:39 +0200

    When submitting a review add it to the database

--- app/src/main/java/com/example/filmapp/activities/WriteReviewActivity.java
+++ app/src/main/java/com/example/filmapp/activities/WriteReviewActivity.java
@@ -11,14 +11,23 @@ import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.filmapp.R;
+import com.example.filmapp.application.repository.MovieReviewRepository;
+import com.example.filmapp.application.viewmodel.MovieReviewViewModel;
+import com.example.filmapp.data.Database;
+import com.example.filmapp.model.MovieReview;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 public class WriteReviewActivity extends AppCompatActivity {
 
     private EditText firstNameEditText;
     private EditText lastNameEditText;
     private EditText reviewEditText;
+    private MovieReviewViewModel movieReviewViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,9 @@ public class WriteReviewActivity extends AppCompatActivity {
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.home_icon_silhouette);
 
         }
+        MovieReviewRepository movieReviewRepository = new MovieReviewRepository(Database.getDatabaseInstance(this), Database.getDatabaseInstance(this).movieReviewDao());
+        movieReviewViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(MovieReviewViewModel.class);
+        movieReviewViewModel.init(movieReviewRepository);
 
         firstNameEditText = findViewById(R.id.writeReviewFirstName);
         lastNameEditText = findViewById(R.id.writeReviewLastName);
@@ -54,16 +66,23 @@ public class WriteReviewActivity extends AppCompatActivity {
     }
 
     public void onSubmitButton(View view) {
+
         String firstName = firstNameEditText.getText().toString().trim();
         String lastName = lastNameEditText.getText().toString().trim();
         String review = reviewEditText.getText().toString().trim();
-
+        String reviewId = UUID.randomUUID().toString();
         // Checks if a field is empty
         if (firstName.isEmpty() || lastName.isEmpty() || review.isEmpty()) {
             Toast.makeText(this, "Please fill in all fields before submitting", Toast.LENGTH_SHORT).show();
         } else {
            // Logic for adding a review to review section/database ??
-            Intent intent = new Intent(this, ReviewOverviewActivity.class);
+            MovieReview movieReview = new MovieReview();
+            movieReview.setReviewContent(review);
+            movieReview.setDateCreated(LocalDate.now().toString());
+            movieReview.setAuthor(firstName + " " + lastName);
+            movieReview.setId(reviewId);
+            movieReviewViewModel.insertMovieReview(movieReview);
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
     }
